4e5e38488cefd27d22e4419f9fffe72d6d7d62b9
luohaosen <luohaosen@oriseq.com>
2024-11-01 09:58:46 +0800

    test.py输出路径修改

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     # 配置文件未产生/需要更新配置文件，下载文件的示例
     os.system(f"python ~/software/tosutil/tosutil_download.py\
             --i tos://skyseq-product-tos/0012v00002ONfrfAAD/JYTK-20241021-L-01-2024-10-240923 \
-            --o /ifs1/data/STS/ \
+            --o ./test/ \
             --c ./tosutil_ssh_key_passwod/.20241031_tosutilconfig \
             --e https://tos-cn-shanghai.volces.com \
             --re cn-shanghai \
@@ -15,6 +15,6 @@ if __name__ == '__main__':
     # 已有配置文件，下载文件的示例
     os.system(f"python ~/software/tosutil/tosutil_download.py \
             --i tos://skyseq-product-tos/0012v00002ONfrfAAD/JYTK-20241025-L-01-2024-10-310113 \
-            --o /ifs1/data/STS/ \
+            --o ./test/ \
             --c ./tosutil_ssh_key_passwod/.20241031_tosutilconfig "
             )
